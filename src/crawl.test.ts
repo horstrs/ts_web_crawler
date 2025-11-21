90b030b4c5bb642732362c292f42a61286b6e996
horstrs <horst.marcelo@gmail.com>
2025-11-21 18:09:29 -0300

    extracted title and first P

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,9 +1,49 @@
 import { expect, test } from "vitest";
-import { normalizeURL } from "./crawl.js";
+import { getFirstParagraphFromHTML, getH1FromHTML, normalizeURL } from "./crawl.js";
 
 test("Normalize URL", () => {
   expect(normalizeURL("https://blog.boot.dev/path/")).toBe("blog.boot.dev/path");
-  expect(normalizeURL("https://blog.boot.dev/path2?query=string")).toBe("blog.boot.dev/path2?query=string");
+  expect(normalizeURL("https://blog.boot.dev/path2?query=string")).toBe("blog.boot.dev/path2");
   expect(normalizeURL("http://user:pass@blog.boot.dev/path3/")).toBe("blog.boot.dev/path3");
   expect(normalizeURL("http://blog.boot.dev:8081/path4")).toBe("blog.boot.dev:8081/path4");
+});
+
+test("getH1FromHTML basic", () => {
+  const inputBody = `<html><body><h1>Test Title</h1></body></html>`;
+  const actual = getH1FromHTML(inputBody);
+  const expected = "Test Title";
+  expect(actual).toEqual(expected);
+});
+
+test("getH1FromHTML no header", () => {
+  const inputBody = `<html><body><p>Paragraph only.</p></body></html>`;
+  const actual = getH1FromHTML(inputBody);
+  const expected = "";
+  expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML main priority", () => {
+  const inputBody = `
+    <html><body>
+      <p>Outside paragraph.</p>
+      <main>
+        <p>Main paragraph.</p>
+      </main>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "Main paragraph.";
+  expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML no main", () => {
+  const inputBody = `
+    <html><body>
+      <p>First paragraph.</p>
+      <p>Second paragraph.</p>
+    </body></html>
+  `;
+  const actual = getFirstParagraphFromHTML(inputBody);
+  const expected = "First paragraph.";
+  expect(actual).toEqual(expected);
 });
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,6 +1,34 @@
+import { JSDOM } from "jsdom";
+
 export function normalizeURL(urlToNormalize: string): string {
   const url = new URL(urlToNormalize);
   const pathname = url.pathname.at(-1) !== "/" ? url.pathname : url.pathname.slice(0, -1)
-  const normalizedUrl = url.host + pathname + url.search;
+  const normalizedUrl = url.host + pathname;
   return normalizedUrl;
+}
+
+export function getH1FromHTML(html: string): string {
+  const htmlObject = new JSDOM(html);
+  const header = htmlObject.window.document.querySelector("h1");
+  if (!header) {
+    return "";
+  }
+  return header.textContent
+}
+
+export function getFirstParagraphFromHTML(html: string): string {
+  const htmlObject = new JSDOM(html);
+  const main = htmlObject.window.document.querySelector("main");
+  if (!main) {
+    return getFirstParagraphFromNode(htmlObject.window.document)
+  }
+  return getFirstParagraphFromNode(main)
+}
+
+function getFirstParagraphFromNode(node: Document | HTMLElement): string {
+  const paragraph = node.querySelector("p");
+  if (!paragraph) {
+      return "";
+    }
+  return paragraph.textContent
 }
